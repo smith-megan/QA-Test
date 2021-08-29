5cdcabfb4a49765c205a710971878283d16566ed
Megan <smith.megan.design@gmail.com>
2021-08-28 23:39:41 -0600

    deeper specificity on tests and additional test added

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -23,19 +23,32 @@ describe('Single game test', async ()=>{
     test('I can place an X in the top left square', async ()=>{
         let X= await(await driver).findElement(By.xpath('//*[@id="cell-0"]'))
         await X.click();
+        let found= await(await driver).findElement(By.xpath('//*[@id="cell-0"]')).getText()
+        expect(found).toEqual("X")
     }) 
     test('I can place an X in the upper right square', async ()=>{
         let X= await(await driver).findElement(By.xpath('//*[@id="cell-2"]'))
         await X.click();
+        
+        let found= await(await driver).findElement(By.xpath('//*[@id="cell-2"]')).getText()
+        expect(found).toEqual("X")
     })
     test('I can place an X in the bottom right square', async ()=>{
         let X= await(await driver).findElement(By.xpath('//*[@id="cell-8"]'))
         await X.click();
+        
+        let found= await(await driver).findElement(By.xpath('//*[@id="cell-8"]')).getText()
+        expect(found).toEqual("X")
     })
       
     test('Computer places an O', async ()=>{
         let board= await(await driver).findElement(By.xpath('//*[@id="cell-1"]')).getText()
         expect(board).toEqual("O")
     })
- 
+    test('I cannot write over another square', async()=>{
+        let squareBefore= await(await driver).findElement(By.xpath('//*[@id="cell-1"]'))
+        await squareBefore.click()
+        let squareAfter=await(await driver).findElement(By.xpath('//*[@id="cell-1"]')).getText()
+        expect(squareAfter).toEqual("O")
+    })
 })
